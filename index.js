60fccdca3994fdcf34769feaacaf279818fdd8aa
angel chavez <chavezangel62@gmail.com>
2024-09-20 11:03:36 -0600

    define variables and do math

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2= 2;
+let multiply = num1 * num2;
+// function multiply(num1,num2){
+//     return num1 * num2;
 
-// Write your code here
+// }
+// console.log(multiply(31,2));    
+let random = num1 + num2;
+
+let num3 = 8;
+let num4 = 4
+let mod = num3 - num4 
+
+let max = 20;
